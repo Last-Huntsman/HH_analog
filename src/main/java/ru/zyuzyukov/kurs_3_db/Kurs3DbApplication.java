ba2a6128c86d14568974b25fbff5925ebb176e20
Egor <zuzik17@yandex.ru>
2025-09-12 09:35:38 +0300

    feat: INITIAL PROJECT

---
+++ src/main/java/ru/zyuzyukov/kurs_3_db/Kurs3DbApplication.java
@@ -0,0 +1,13 @@
+package ru.zyuzyukov.kurs_3_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kurs3DbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kurs3DbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/zyuzyukov/kurs_3_db/Kurs3DbApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zyuzyukov.kurs_3_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kurs3DbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
