567c4b5be75f9e5f69fb66b2b9e2bc14f5ca3e4e
Egor <zuzik17@yandex.ru>
2025-09-14 15:47:47 +0300

    feat: release mapper for worker with documentation

--- src/main/java/ru/zyuzyukov/kurs_3_db/mapper/WorkerMapper.java
+++ src/main/java/ru/zyuzyukov/kurs_3_db/mapper/WorkerMapper.java
@@ -2,16 +2,40 @@ package ru.zyuzyukov.kurs_3_db.mapper;
 
 import org.springframework.stereotype.Component;
 import ru.zyuzyukov.kurs_3_db.dto.WorkerDto;
+import ru.zyuzyukov.kurs_3_db.entity.Employer;
+import ru.zyuzyukov.kurs_3_db.entity.Skill;
 import ru.zyuzyukov.kurs_3_db.entity.Worker;
+import ru.zyuzyukov.kurs_3_db.service.BaseService;
+
+import java.util.List;
+
 @Component
 public class WorkerMapper implements  Mapper<WorkerDto, Worker> {
+    private final BaseService<Skill> skillService;
+
+    public WorkerMapper(BaseService<Skill> skillService) {
+        this.skillService = skillService;
+    }
+
+
     @Override
     public WorkerDto toDto(Worker entity) {
-        return null;
+        return new WorkerDto(
+                entity.getId(),
+                entity.getName(),
+                entity.getExperience(),
+                entity.getWorkerSkills().stream().map(Skill::getId).toList()
+        );
     }
 
     @Override
     public Worker toCreateEntity(WorkerDto dto) {
-        return null;
+        List<Skill> skills = skillService.findAllById(dto.getWorkerSkillsId());
+        return new Worker(
+                null,
+                dto.getName(),
+                dto.getExperience(),
+                skills
+        );
     }
 }
