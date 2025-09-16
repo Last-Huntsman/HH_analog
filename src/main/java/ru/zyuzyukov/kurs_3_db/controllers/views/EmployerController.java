94ccf6fd9f825531d8c21086e1d0ffc987693670
Egor <zuzik17@yandex.ru>
2025-09-16 15:20:35 +0300

    feat: Added functional for getting vacancy for one employer. Unworked edit and delete

--- src/main/java/ru/zyuzyukov/kurs_3_db/controllers/views/EmployerController.java
+++ src/main/java/ru/zyuzyukov/kurs_3_db/controllers/views/EmployerController.java
@@ -1,30 +1,52 @@
 package ru.zyuzyukov.kurs_3_db.controllers.views;
 
-import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import ru.zyuzyukov.kurs_3_db.dto.EmployerDto;
 import ru.zyuzyukov.kurs_3_db.entity.Employer;
+import ru.zyuzyukov.kurs_3_db.entity.Vacancy;
 import ru.zyuzyukov.kurs_3_db.mapper.EmployerMapper;
-import ru.zyuzyukov.kurs_3_db.service.BaseService;
+import ru.zyuzyukov.kurs_3_db.service.EmployerService;
+import ru.zyuzyukov.kurs_3_db.service.VacancyService;
 
 import java.util.UUID;
-import java.util.function.Supplier;
 
 @Controller("employerViewController")
 @RequestMapping("/view/employer")
+
 public class EmployerController extends BaseViewController<EmployerDto, Employer> {
-    public EmployerController(BaseService<Employer> baseService, EmployerMapper mapper) {
-        super(baseService,
+    private final VacancyService vacancyService;
+    private final EmployerService employerService;
+
+    public EmployerController(EmployerService employerService, EmployerMapper mapper, VacancyService vacancyService) {
+        super(employerService,
                 mapper,
-                 EmployerDto::new,
+                EmployerDto::new,
                 "employer",
                 "employers");
+        this.vacancyService = vacancyService;
+        this.employerService = employerService;
     }
+
+    @GetMapping("{id}/vacancies")
+    public String listVacanciesForEmployer(@PageableDefault(size = 10, sort = "id") Pageable pageable,
+                       Model model, @PathVariable UUID id) {
+        Employer employer = employerService.findById(id).orElseThrow(() -> new IllegalArgumentException("Employer not found"));
+        Page<Vacancy> vacancies = vacancyService.findAllByEmployerId(id, pageable);
+        model.addAttribute("vacancies", vacancies.getContent());
+        model.addAttribute("page", vacancies);
+        model.addAttribute("id", id);
+        model.addAttribute("employer", employer);
+
+
+        return "employer/vacancies";
+    }
+
 
 }
--- src/main/java/ru/zyuzyukov/kurs_3_db/repositories/VacancyRepository.java
+++ src/main/java/ru/zyuzyukov/kurs_3_db/repositories/VacancyRepository.java
@@ -1,10 +1,14 @@
 package ru.zyuzyukov.kurs_3_db.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.zyuzyukov.kurs_3_db.entity.Vacancy;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface VacancyRepository extends JpaRepository<Vacancy, UUID> {
+   Page<Vacancy> findByEmployerId(UUID employerId, Pageable pageable);
 
 }
