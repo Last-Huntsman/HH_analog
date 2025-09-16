ccecdbad7f7ac9fa44e261067daadbb214b6615f
Egor <zuzik17@yandex.ru>
2025-09-16 12:10:01 +0300

    feat: Created BaseViewController with Employer Controller for web with views

--- src/main/java/ru/zyuzyukov/kurs_3_db/controllers/views/BaseViewController.java
+++ src/main/java/ru/zyuzyukov/kurs_3_db/controllers/views/BaseViewController.java
@@ -1,4 +1,92 @@
-package ru.zyuzyukov.kurs_3_db.controllers;
+package ru.zyuzyukov.kurs_3_db.controllers.views;
 
-public class BaseViewController {
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import ru.zyuzyukov.kurs_3_db.dto.CRUDable;
+import ru.zyuzyukov.kurs_3_db.entity.Entitytable;
+import ru.zyuzyukov.kurs_3_db.mapper.Mapper;
+import ru.zyuzyukov.kurs_3_db.service.BaseService;
+
+import java.util.UUID;
+import java.util.function.Supplier;
+
+public abstract class BaseViewController<T extends CRUDable, D extends Entitytable> {
+
+    private final BaseService<D> baseService;
+    private final Mapper<T, D> mapper;
+    private final Supplier<T> dtoSupplier; // Для создания нового DTO
+    private final String smallName;        // Например "employer"
+    private final String longName;         // Например "employers"
+
+    public BaseViewController(BaseService<D> baseService,
+                              Mapper<T, D> mapper,
+                              Supplier<T> dtoSupplier,
+                              String smallName,
+                              String longName) {
+        this.baseService = baseService;
+        this.mapper = mapper;
+        this.dtoSupplier = dtoSupplier;
+        this.smallName = smallName;
+        this.longName = longName;
+    }
+
+
+    @GetMapping
+    public String list(@PageableDefault(size = 10, sort = "name") Pageable pageable,
+                       Model model) {
+        Page<T> page = baseService.findAll(pageable).map(mapper::toDto);
+        model.addAttribute(longName, page.getContent());
+        model.addAttribute("page", page);
+        return smallName + "/list";
+    }
+
+
+    @GetMapping("/create")
+    public String showCreateForm(Model model) {
+        model.addAttribute( dtoSupplier.get());
+        return smallName + "/create";
+    }
+
+
+    @PostMapping("/create")
+    public  String create(@Valid @ModelAttribute T dto,
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return smallName + "/create";
+        }
+        baseService.save(mapper.toCreateEntity(dto));
+        return "redirect:/view/" + smallName;
+    }
+
+
+    @GetMapping("/edit/{id}")
+    public String showEditForm(@PathVariable UUID id, Model model) {
+        D entity = baseService.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException(smallName + " not found"));
+        model.addAttribute( mapper.toDto(entity));
+        return smallName + "/edit";
+    }
+
+
+    @PostMapping("/edit")
+    public String update(@Valid @ModelAttribute T dto,
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return smallName + "/edit";
+        }
+        baseService.update(mapper.toCreateEntity(dto));
+        return "redirect:/view/" + smallName;
+    }
+
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable UUID id) {
+        baseService.delete(id);
+        return "redirect:/view/" + smallName;
+    }
 }
